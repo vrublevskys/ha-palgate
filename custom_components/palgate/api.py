f90203fce246370668d0e6f6054acc157a100df0
sergey.vrublevsky <sergey.vrublevsky@bpmobile.com>
2024-05-28 20:01:34 +0300

    Move lib

--- custom_components/palgate/api.py
+++ custom_components/palgate/api.py
@@ -27,7 +27,7 @@ class PalgateApiClient:
         session: Optional[aiohttp.client.ClientSession] = None,
     ) -> None:
         """Initialize connection with Palgate."""
-        self.nativeLib = cdll.LoadLibrary('./lib/libnative-lib.so')
+        self.nativeLib = cdll.LoadLibrary('./libnative-lib.so')
 
         self._session = session
         self.device_id: str = device_id
